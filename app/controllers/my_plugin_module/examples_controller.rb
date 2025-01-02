e04cf6a51b9ccbc5b5d48b4c81239ce316dbfc0f
mihirranjan7 <mihirranjan730@gmail.com>
2025-01-03 02:55:27 +0530

    Update examples_controller.rb

--- app/controllers/my_plugin_module/examples_controller.rb
+++ app/controllers/my_plugin_module/examples_controller.rb
@@ -1,11 +1,20 @@
-# frozen_string_literal: true
-
-module ::MyPluginModule
-  class ExamplesController < ::ApplicationController
-    requires_plugin PLUGIN_NAME
-
-    def index
-      render json: { hello: "world" }
+# app/jobs/indexnow_batch_ping.rb
+module Jobs
+  class IndexnowBatchPing < ::Jobs::Base
+    def execute(args)
+      # Fetch topics created in the last 30 days (you can modify the logic as needed)
+      topics = Topic.where("created_at > ?", 30.days.ago).limit(100)
+      
+      topics.each do |topic|
+        # Construct the URL for each topic
+        url = "#{Discourse.base_url}/t/#{topic.slug}/#{topic.id}"
+        
+        # Log the URL being pinged
+        Rails.logger.info("[IndexNow Plugin] Pinging URL: #{url}")
+        
+        # Call the ping method to notify IndexNow
+        IndexNowPlugin.ping_indexnow(url)
+      end
     end
   end
 end
